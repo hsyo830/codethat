0e38c0f0653112d5e6fae889234c3d5a0a1e2934
황서영 <nct830@naver.com>
2025-11-08 02:37:16 +0900

    feat : 커뮤니티 페이지 추가 (Link로 이동하기)

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './components/App';
-import HomePage from './pages/HomePage';
-import CoursePage from './pages/CoursePage';
-import CourseListPage from './pages/CourseListPage';
-import WishlistPage from './pages/WishlistPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./components/App";
+import HomePage from "./pages/HomePage";
+import CoursePage from "./pages/CoursePage";
+import CourseListPage from "./pages/CourseListPage";
+import QuestionPage from "./pages/QuestionPage";
+import QuestionListPage from "./pages/QuestionListPage";
+import WishlistPage from "./pages/WishlistPage";
 
 function Main() {
   return (
@@ -16,6 +18,8 @@ function Main() {
             path="courses/react-frontend-development"
             element={<CoursePage />}
           />
+          <Route path="questions" element={<QuestionListPage />} />
+          <Route path="questions/616825" element={<QuestionPage />} />
           <Route path="wishlist" element={<WishlistPage />} />
         </Routes>
       </App>
