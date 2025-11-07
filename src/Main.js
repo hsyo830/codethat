bc0e9219872e5024467637a8b5ec23ed800a1ad7
황서영 <nct830@naver.com>
2025-11-08 06:12:25 +0900

    feat : 동적 경로 생성 / 존재하지 않는 페이지 생성

--- src/Main.js
+++ src/Main.js
@@ -6,6 +6,7 @@ import CourseListPage from "./pages/CourseListPage";
 import QuestionPage from "./pages/QuestionPage";
 import QuestionListPage from "./pages/QuestionListPage";
 import WishlistPage from "./pages/WishlistPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function Main() {
   return (
@@ -15,13 +16,14 @@ function Main() {
           <Route index element={<HomePage />} />
           <Route path="courses">
             <Route index element={<CourseListPage />} />
-            <Route path="react-frontend-development" element={<CoursePage />} />
+            <Route path=":courseSlug" element={<CoursePage />} />
           </Route>
           <Route path="questions">
             <Route index element={<QuestionListPage />} />
-            <Route path="616825" element={<QuestionPage />} />
+            <Route path=":questionId" element={<QuestionPage />} />
           </Route>
           <Route path="wishlist" element={<WishlistPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
